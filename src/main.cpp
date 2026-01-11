da6ccff72c3aabd6fd96f71ea210b40387c2b13e
kibet-rotich <asbelkibetrotich@gmail.com>
2026-01-11 05:34:58 +0300

    type command added

--- src/main.cpp
+++ src/main.cpp
@@ -17,7 +17,22 @@ int main(int argc, char *argv[]) {
 
     if(command == "exit"){
       exit(0);
-    }else if(command.substr(0,5) == "echo "){
+    }else if(command.substr(0,5)=="type "){
+      string validCommands[] ={"exit","echo","type"};
+      string inputcommand = command.substr(5);
+      bool found = false;
+      for(int i =0; i<validCommands->size();i++){
+        if(validCommands[i]==inputcommand){
+          cout<<inputcommand<<" is a shell builtin"<<endl;
+          found = true;
+        }
+      }
+
+      if(!found){
+        cout<<inputcommand<<" not found"<<endl;
+      }
+    }
+    else if(command.substr(0,5) == "echo "){
       string output  = command.substr(5);
       cout<<output<<endl;
       
