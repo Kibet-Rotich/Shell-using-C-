695cc8aadbbc4ac5651a164a6c2f99671cd7d828
kibet-rotich <asbelkibetrotich@gmail.com>
2026-01-11 04:44:02 +0300

    First command

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include <string>
 
 int main() {
+  using namespace std;
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  string command;
+
+  getline(cin, command);
+
+  cout<< command<< ": Command not found"<< endl;
 }
