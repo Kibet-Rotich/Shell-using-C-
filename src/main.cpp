6e142fa81713f084892eabb3b3f05f221fd7221f
Kibet-Rotich <kibet.rotich2022@students.jkuat.ac.ke>
2026-01-16 22:07:41 +0300

    type command, augmented

--- src/main.cpp
+++ src/main.cpp
@@ -5,15 +5,18 @@
 #include<vector>
 #include<sstream>
 #include<filesystem>
+#include<unordered_set>
+#include <bits/basic_string.h>
 
 using namespace std;
 namespace fs = std::filesystem;
 
-
+//function to check if a file is executable
 bool isexecutable(const string& path){
   return access(path.c_str(),X_OK)==0;
 }
 
+//function to get directories from PATH environment variable
 vector<string> getpathdirs(){
   vector<string> dirs;
   const char* PATH = getenv("PATH");
@@ -31,60 +34,72 @@ vector<string> getpathdirs(){
   return dirs;
 }
 
+//function to find command in PATH directories
+string find_in_path(const string& command)
+{
+  vector<string> pathDirs = getpathdirs();
+
+  for(const string& dir : pathDirs){
+    fs::path fullpath = fs::path(dir) / command;
+    if(fs::exists(fullpath) && isexecutable(fullpath.string())){
+      return fullpath.string();
+    }
+  }
+  return "";
+}
+
+
+
 int main(int argc, char *argv[]) {
   
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
+
   while(true){
     std::cout << "$ ";
 
     string command;
 
     getline(cin, command);
-
+//check if the user wants to exit
     if(command == "exit"){
       exit(0);
-    }else if(command.substr(0,5)=="type "){
-      string validCommands[] ={"exit","echo","type"};
+    }
+//check if the user wants to use the type command   
+    if(command.substr(0,5)=="type "){
+      unordered_set<string> validCommands ={"exit","echo","type"};
       string inputcommand = command.substr(5);
       bool found = false;
-
-      for(int i =0; i<size(validCommands);i++){
-        if(validCommands[i]==inputcommand){
-          cout<<inputcommand<<" is a shell builtin"<<endl;
-          found = true;
-        }
+      
+      int i =0;
+      //check built-in commands
+      if(validCommands.contains(inputcommand)){
+        cout<<inputcommand<<" is a shell built-in"<<endl;
+        found = true;
       }
 
-      if(!found){
-        vector<string> dirs = getpathdirs();
-        for(const string& dir: dirs){
-          fs::path fullpath = fs::path(dir) / inputcommand;
 
-          if(fs::exists(fullpath)&&fs::is_regular_file(fullpath)){
-            if(isexecutable(fullpath)){
-              cout<<inputcommand<< " is "<<fullpath<<endl;
-              found = true;
-            }
-            
-            break;
-            
-          }
-        }
+      //search for user command in PATH directories
+      string pathResult = find_in_path(inputcommand);
+      if(!pathResult.empty()){
+        cout<<inputcommand<<" is "<<pathResult<<endl;
+        found = true;
+        
       }
       if(!found){
-        cout<<inputcommand<<" not found"<<endl;
+        cout<<inputcommand<<": not found"<<endl;
       }
     }
+    //check if the user wants to use the echo command
     else if(command.substr(0,5) == "echo "){
       string output  = command.substr(5);
       cout<<output<<endl;
       
          
     }
+    //command not found case
     else{
       cout<< command<< ": command not found"<< endl;
     }
